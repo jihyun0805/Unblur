b9450323fc74670775ae5518fa618457d642d3ee
softdrink2025 <softdrink2025@gmail.com>
2026-01-14 15:53:15 +0900

    Init: 백엔드 프로젝트 초기 설정

---
+++ BE/src/main/java/com/ssafy/unblur/UnblurApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.unblur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnblurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnblurApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/ssafy/unblur/UnblurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.unblur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnblurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
