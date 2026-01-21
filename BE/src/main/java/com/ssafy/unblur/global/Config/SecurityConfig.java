db6fd2b7d49e46531fe660ce93dbc823bd3e7eee
한상민 <thead97@naver.com>
2026-01-21 21:14:23 +0900

    feat : Config(CORS 관련 WebConfig에 따로 정의)

---
+++ BE/src/main/java/com/ssafy/unblur/global/Config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.ssafy.unblur.global.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean // 비밀번호 암호화
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults()) // CORS
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보안 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // 스프링 시큐리티가 기본으로 제공하는 HTML 로그인 화면을 사용하지 않음
+                .httpBasic(AbstractHttpConfigurer::disable) // 브라우저 팝업창 형태의 기본 인증 방식을 사용하지 않고, 요청 헤더의 토큰을 사용할 것이므로 비활성화
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 서버가 사용자의 세션을 메모리에 저장하지 않도록 'STATELESS(무상태)' 모드로 설정
+                .authorizeHttpRequests(auth -> auth
+                        // Swagger 관련 경로
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                        // 로그인 및 회원가입 경로
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        // 일단은 모든 경로에 대하여 허용
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+}
