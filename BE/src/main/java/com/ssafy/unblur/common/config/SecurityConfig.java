e18e085de02550f2cfed92d75a14f2111e88a05b
한상민 <thead97@naver.com>
2026-01-25 21:42:17 +0900

    feat : 시큐리티 정책 및 유틸리티(S14P11A705-142)

--- BE/src/main/java/com/ssafy/unblur/common/config/SecurityConfig.java
+++ BE/src/main/java/com/ssafy/unblur/common/config/SecurityConfig.java
@@ -1,24 +1,38 @@
 package com.ssafy.unblur.common.config;
 
+import com.ssafy.unblur.common.security.jwt.JWTFilter;
+import com.ssafy.unblur.common.security.jwt.JWTUtil;
+import com.ssafy.unblur.domain.user.repository.UserRepository;
+import com.ssafy.unblur.domain.user.service.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JWTUtil jwtUtil;
+
     @Bean // 비밀번호 암호화
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -38,6 +52,7 @@ public class SecurityConfig {
                         // 일단은 모든 경로에 대하여 허용
                         .anyRequest().permitAll()
                 )
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
