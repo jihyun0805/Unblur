773ce30f3737c5c0402c8d2a00013c5dab0e47f4
namsh1125 <namsh1125@naver.com>
2026-01-25 21:17:40 +0900

    feat: 스케줄링 설정 클래스 구현 (S14P11A705-155)

---
+++ BE/src/main/java/com/ssafy/unblur/common/config/SchedulingConfig.java
@@ -0,0 +1,12 @@
+package com.ssafy.unblur.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 스케줄링 설정 클래스
+ */
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
