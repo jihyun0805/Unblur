cca7176826f91bc5a2ba57d6ec7910121e26371b
한상민 <thead97@naver.com>
2026-01-27 14:28:18 +0900

    feat : 프로필 수정 메서드 추가(S14P11A705-78)

--- BE/src/main/java/com/ssafy/unblur/domain/user/service/UserService.java
+++ BE/src/main/java/com/ssafy/unblur/domain/user/service/UserService.java
@@ -6,6 +6,7 @@ import com.ssafy.unblur.domain.auth.model.User;
 import com.ssafy.unblur.domain.auth.service.AuthService;
 import com.ssafy.unblur.domain.auth.service.RefreshTokenService;
 import com.ssafy.unblur.domain.user.dto.UserProfileResponseDto;
+import com.ssafy.unblur.domain.user.dto.UserProfileUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,6 +38,29 @@ public class UserService {
     public UserProfileResponseDto getMyProfile(String email) {
         User user = authService.findUserByEmail(email)
                 .orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+
+        // 토큰은 살아있지만 DB상에선 탈퇴했을 경우 대비
+        if (!user.isActive()) {
+            throw new BaseException(ErrorCode.INACTIVE_USER);
+        }
+
+        return UserProfileResponseDto.from(user);
+    }
+
+    /**
+     * 현재 로그인된 사용자의 프로필 정보를 수정합니다.
+     */
+    @Transactional
+    public UserProfileResponseDto updateMyProfile(String email, UserProfileUpdateRequestDto dto) {
+        User user = authService.findUserByEmail(email)
+                .orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+
+        // 토큰은 살아있지만 DB상에선 탈퇴했을 경우 대비
+        if (!user.isActive()) {
+            throw new BaseException(ErrorCode.INACTIVE_USER);
+        }
+
+        user.updateProfile(dto);
 
         return UserProfileResponseDto.from(user);
     }
