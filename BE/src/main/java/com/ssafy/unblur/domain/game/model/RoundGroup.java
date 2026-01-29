a62397c533a86bbd553ff9f259ae6ee621d221e8
한상민 <thead97@naver.com>
2026-01-30 00:24:40 +0900

    feat : 랜덤 질문 생성 엔티티(S14P11A705-100)

---
+++ BE/src/main/java/com/ssafy/unblur/domain/game/model/RoundGroup.java
@@ -0,0 +1,16 @@
+package com.ssafy.unblur.domain.game.model;
+
+/**
+ * 질문이 사용되는 라운드 구간.
+ */
+public enum RoundGroup {
+
+    /** 1라운드용 */
+    ROUND_1,
+
+    /** 2라운드용 */
+    ROUND_2,
+
+    /** 3~4라운드용 */
+    ROUND_3_4
+}
