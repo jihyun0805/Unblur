4117909cc54a7b59e27d44d164221e386a0a5464
JiHyun <cjhapple0805@gmail.com>
2026-01-30 17:00:21 +0900

    fix : 알림창 수정(S14P11A705-182)

--- FE/components/home-page.tsx
+++ FE/components/home-page.tsx
@@ -32,7 +32,7 @@ export function HomePage() {
 
   const handleRequestChat = (userId: string) => {
     setTimeout(() => {
-      toast({
+      const { dismiss } = toast({
         title: "1:1 매칭 수락!",
         description: "상대방이 매칭 요청을 수락했습니다. 매칭방으로 이동하시겠습니까?",
         duration: Number.POSITIVE_INFINITY,
@@ -40,6 +40,7 @@ export function HomePage() {
           <Button
             size="sm"
             onClick={() => {
+              dismiss()
               setPendingSessionId(userId)
               setShowCameraTest(true)
             }}
