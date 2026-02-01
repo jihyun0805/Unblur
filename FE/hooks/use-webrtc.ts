18eb80716f2bb6b5b40399c7a95004de6d45f105
김승상 <rovin054@gmail.com>
2026-02-01 22:29:10 +0900

    fix : STUN 제거. TURN 강제

--- FE/hooks/use-webrtc.ts
+++ FE/hooks/use-webrtc.ts
@@ -27,18 +27,29 @@ export interface UseWebRTCReturn {
   isVideoEnabled: boolean
 }
 
-const ICE_SERVERS: RTCConfiguration = {
-  iceServers: [
-    { urls: ["stun:i14a705.p.ssafy.io:8000"] },
+// const ICE_SERVERS: RTCConfiguration = {
+//   iceServers: [
+//     { urls: ["stun:i14a705.p.ssafy.io:8000"] },
+//
+//     // TURN
+//     {
+//       urls: ["turn:i14a705.p.ssafy.io:8000?transport=tcp"],
+//       username: "A705",
+//       credential: "wQ9pX3!Zt7b#V2mN4sC8"
+//     }
+//   ]
+// }
 
-    // TURN
-    {
-      urls: ["turn:i14a705.p.ssafy.io:8000?transport=tcp"],
-      username: "A705",
-      credential: "wQ9pX3!Zt7b#V2mN4sC8"
-    }
-  ]
-}
+const ICE_SERVERS: RTCConfiguration = {
+    iceServers: [
+        {
+            urls: ["turn:i14a705.p.ssafy.io:8000?transport=tcp"],
+            username: "A705",
+            credential: "wQ9pX3!Zt7b#V2mN4sC8",
+        },
+    ],
+    iceTransportPolicy: "relay", // TURN만 쓰게 강제(테스트용)
+};
 
 export function useWebRTC({
   sessionId,
